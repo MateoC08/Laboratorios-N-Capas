3163e2e35cb7743e650716bf4c8f897c3bf16285
unknown <00027922@uca.edu.sv>
2026-05-05 10:53:24 -0600

    Agregando variables de entorno y modificando yaml

---
+++ Lab02Final/src/main/java/com/example/lab02final/Lab02FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab02final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab02FinalApplication.class, args);
+    }
+
+}
---
+++ Lab02Final/src/test/java/com/example/lab02final/Lab02FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab02final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
